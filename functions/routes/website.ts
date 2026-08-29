0faf2ffdfd08d3bd1991622e98b294252039e39d
tvivancoIMEB <tomas.vivanco@municipalidadelbosque.cl>
2026-08-28 22:04:23 -0400

    Update redirect logic for missing short link keys
    Refactor redirect logic for non-existent keys to redirect to the origin URL instead of a fixed URL.

--- functions/routes/website.ts
+++ functions/routes/website.ts
@@ -35,18 +35,34 @@ website.get('/', async (ctx) => {
   return ctx.html(Home(props));
 });
 
-website.get('/:id', async (ctx) => {
-  const key = ctx.req.param('id');
-  const link: ShortLink = await ctx.env.SHORTLINKS.get(key, {
-    type: 'json',
-  });
+website.get('/', async (ctx) => {
+    const { cf } = ctx.req.raw;
+    const origin = new URL(ctx.req.url).origin;
+    const data = [
+      {
+        key: 'github',
+        url: 'https://github.com/vickyrathee/idm',
+        short_url: `${origin}/github`,
+        params: {
+          url_medium: 'search',
+          utm_campaign: 'website',
+        },
+        created_at: new Date(),
+      },
+    ];
+    ...
 
-  if (link && link.url) {
-    const url = appendParamsToURL(link.url, link.params);
-    return ctx.redirect(url, 302);
-  } else {
-    return ctx.redirect('https://idm.in/', 302);
-  }
-});
+  Y en el redirect por defecto cuando no existe el key:
+  website.get('/:id', async (ctx) => {
+    const key = ctx.req.param('id');
+    const link: ShortLink = await ctx.env.SHORTLINKS.get(key, {
+      type: 'json',
+    });
 
-export { website };
+    if (link && link.url) {
+      const url = appendParamsToURL(link.url, link.params);
+      return ctx.redirect(url, 302);
+    } else {
+      const origin = new URL(ctx.req.url).origin;
+      return ctx.redirect(origin, 302);
+    }
